6c4f6a3956f3f2ba8649004ba4d8247bf814ddd0
viraj96 <parimiviraj@gmail.com>
2018-03-31 20:18:40 +0530

    Added workload sensitive part of project

--- cotton-runtime.cpp
+++ cotton-runtime.cpp
@@ -6,10 +6,15 @@
 #include "cotton.h"
 #include "cotton-runtime.h"
 
+
+unsigned int cotton::Deque::sizeof_deque() {
+	return abs(tail - head);
+}
+
 /**
 Pushes the task to the deque data structure after checking boundary conditions
 
-@param lambda Takes the task encapsulated in a lamda function
+@param task Takes the task encapsulated in a lamda function
 @return Void
 **/
 void cotton::Deque::push_to_deque(void *task) {
@@ -140,7 +145,7 @@ Finds a task from the per thread deque data structures, executes it and finally 
 @return Void
 **/
 void cotton::find_and_execute_task() {
-	void * task_ptr = cotton::grab_task_from_runtime();
+	void* task_ptr = cotton::grab_task_from_runtime();
 	if( task_ptr != NULL ) {
 		std::function<void()> task = *(std::function<void()> *)task_ptr;
 		task();
@@ -155,9 +160,10 @@ Pushes a task encapsulated in a lambda function into the calling thread's data s
 
 @return Void
 **/
-void cotton::push_task_to_runtime(void *task) {
-	unsigned int current_thread_id = cotton::get_threadID();
-	cotton::DEQUE_ARRAY[ current_thread_id ].push_to_deque( task );
+void cotton::push_task_to_runtime(void* task) {
+	unsigned int current_worker_id = cotton::get_threadID();
+	cotton::WORKER_ARRAY[ current_worker_id ].worker_deque->push_to_deque( task );
+	cotton::workload_up_check(current_worker_id);
 }
 
 /**
@@ -168,20 +174,30 @@ It first checks in the calling thread deque data structure and if if does not fi
 @return Task encapsulated in a lambda function 
 **/
 void* cotton::grab_task_from_runtime() {
-	unsigned int current_thread_id = cotton::get_threadID();
+	unsigned int current_worker_id = cotton::get_threadID();
 	
-	assert((pthread_mutex_lock( &cotton::DEQUE_MUTEX[ current_thread_id ] ) == 0));
-	void *grabbed_task_ptr = cotton::DEQUE_ARRAY[ current_thread_id ].pop_from_deque();
-	assert((pthread_mutex_unlock( &cotton::DEQUE_MUTEX[ current_thread_id ] ) == 0));
+	assert((pthread_mutex_lock( &cotton::DEQUE_MUTEX[ current_worker_id ] ) == 0));
+	void *grabbed_task_ptr = cotton::WORKER_ARRAY[ current_worker_id ].worker_deque->pop_from_deque();
+	assert((pthread_mutex_unlock( &cotton::DEQUE_MUTEX[ current_worker_id ] ) == 0));
 	
 	if( grabbed_task_ptr == NULL ) {
-		unsigned int random_deque_id = current_thread_id;
-		while( random_deque_id == current_thread_id ) {
-			random_deque_id = rand() % cotton::NUM_WORKERS;
+		if (cotton::WORKER_ARRAY[current_worker_id].is_victim())
+			cotton::end_victim(current_worker_id);
+		unsigned int random_worker_id = current_worker_id;
+		while( random_worker_id == current_worker_id ) {
+			random_worker_id = rand() % cotton::NUM_WORKERS;
 		}
-		assert((pthread_mutex_lock( &cotton::DEQUE_MUTEX[ random_deque_id ] ) == 0));
-		grabbed_task_ptr = cotton::DEQUE_ARRAY[ random_deque_id ].steal_from_deque();
-		assert((pthread_mutex_unlock( &cotton::DEQUE_MUTEX[ random_deque_id ] ) == 0));
+		assert((pthread_mutex_lock( &cotton::DEQUE_MUTEX[ random_worker_id ] ) == 0));
+		grabbed_task_ptr = cotton::WORKER_ARRAY[ random_worker_id ].worker_deque->steal_from_deque();
+		if (grabbed_task_ptr != NULL) {
+			cotton::begin_victim_thief_relationship(random_worker_id, current_worker_id);
+			// random is victim right?
+			cotton::workload_down_check(random_worker_id);
+		}
+		assert((pthread_mutex_unlock( &cotton::DEQUE_MUTEX[ random_worker_id ] ) == 0));
+	}
+	else {
+		cotton::workload_down_check(current_worker_id);
 	}
 
 	return grabbed_task_ptr;
@@ -203,7 +219,8 @@ void cotton::init_runtime() {
 
 	assert((pthread_setspecific(cotton::THREAD_KEY, args) == 0));
 
-	cotton::DEQUE_ARRAY = new cotton::Deque[cotton::NUM_WORKERS];	
+	// cotton::DEQUE_ARRAY = new cotton::Deque[cotton::NUM_WORKERS];	
+	cotton::WORKER_ARRAY = new cotton::Worker[cotton::NUM_WORKERS];	
 	cotton::thread = (pthread_t *)malloc(sizeof(pthread_t) * cotton::NUM_WORKERS);
 	cotton::DEQUE_MUTEX = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * cotton::NUM_WORKERS);
 	for(unsigned int i = 0; i < cotton::NUM_WORKERS; i++) {
@@ -260,7 +277,7 @@ Call free() on everything allocated on heap.
 void cotton::free_all() {
 	free(cotton::thread);
 	free(cotton::DEQUE_MUTEX);
-	delete[] cotton::DEQUE_ARRAY;
+	delete[] cotton::WORKER_ARRAY;
 } 
 
 /**
@@ -275,4 +292,159 @@ void cotton::finalize_runtime() {
 	}
 
 	cotton::free_all();
+}
+
+/**
+Checks if the worker is still a victim. A worker will remain a victim until its thief list points to NULL.
+
+@return True, if worker is a victim. Else, false.
+**/
+bool cotton::Worker::is_victim() {
+	return (thief_list_head->next != NULL);
+}
+
+/**
+Add a thief node to the thief list of worker. This establishes a victim-thief relationship.
+
+@param thief_worker_id Thief worker
+@return Void
+**/
+void cotton::Worker::add_thief(int thief_worker_id) {
+	cotton::Thief_node* new_thief_node = new cotton::Thief_node(thief_worker_id);
+	new_thief_node->next = thief_list_head->next;
+	if (new_thief_node->next != NULL)
+		new_thief_node->next->prev = new_thief_node;
+	new_thief_node->prev = thief_list_head;
+	thief_list_head->next = new_thief_node;
+}
+
+/**
+Iteratively free the memory occupied by the thief list of the worker.
+
+@return Void
+**/
+void cotton::Worker::free_thief_list() {
+	cotton::Thief_node* cur_node = thief_list_head;
+	while (cur_node->next != NULL)
+		cur_node = cur_node->next;
+	while (cur_node != thief_list_head) {
+		cotton::Thief_node* temp = cur_node;
+		cur_node = cur_node->prev;
+		delete temp;
+	}
+	thief_list_head->next = NULL;
+
+	// printf("check if empty %d %d\n", thief_list_head->next == NULL, cur_node == thief_list_head);
+}
+
+/**
+Begin a victim-thief relationship by:
+- adding a new thief node to the worker's thief list.
+- reducing the CPU frequency of thief worker using DVFS.
+
+@param victim_worker_id Victim worker
+@param thief_worker_id Thief worker, the one which stole the task.
+
+@return Void
+**/
+void cotton::begin_victim_thief_relationship(int victim_worker_id, int thief_worker_id) {
+	cotton::WORKER_ARRAY[victim_worker_id].add_thief(thief_worker_id);
+	DOWN(thief_worker_id);
+}
+
+/**
+Increasing the CPU frequency of all thiefs, and their thieves, recursively by one level.
+
+@param victim_worker_id Victim worker
+@param thief_worker_id Thief worker, the one which stole the task.
+
+@return Void
+**/
+void cotton::end_victim_thief_relationship(int victim_worker_id, int thief_worker_id) {
+	UP(thief_worker_id);
+	cotton::Thief_node* cur_node = cotton::WORKER_ARRAY[thief_worker_id].thief_list_head;
+	while (cur_node->next != NULL) {
+		cur_node = cur_node->next;
+		int thief_thief = cur_node->thief_worker_id;
+		cotton::end_victim_thief_relationship(thief_worker_id, thief_thief);
+	}
+}
+
+/**
+End the victim status of a worker:
+- increasing the CPU frequency of all its thiefs, and their thieves, recursively by one level.
+- free the memory occupied by the thief list of the worker
+
+@param victim_worker_id Victim worker
+
+@return Void
+**/
+void cotton::end_victim(int victim_worker_id) {
+	cotton::Thief_node* cur_node = cotton::WORKER_ARRAY[victim_worker_id].thief_list_head;
+	while (cur_node->next != NULL) {
+		cur_node = cur_node->next;
+		int thief = cur_node->thief_worker_id;
+		cotton::end_victim_thief_relationship(victim_worker_id, thief);
+	}
+	cotton::WORKER_ARRAY[victim_worker_id].free_thief_list();
+}
+
+void cotton::workload_up_check(int worker_id) {
+	int current_threshold_index = cotton::WORKER_ARRAY[ worker_id ].current_size_threshold_index;
+	int current_threshold = cotton::WORKER_ARRAY[ worker_id ].size_thresholds[current_threshold_index];
+
+	if( cotton::WORKER_ARRAY[ worker_id ].worker_deque->sizeof_deque() > current_threshold ) {
+		if( current_threshold_index < NUM_THRESHOLDS - 1 ) {
+			cotton::WORKER_ARRAY[ worker_id ].current_size_threshold_index += 1;
+			UP(worker_id);
+		}
+	}
+}
+
+void cotton::workload_down_check(int worker_id) {
+	int current_threshold_index = cotton::WORKER_ARRAY[ worker_id ].current_size_threshold_index;
+	int current_threshold = cotton::WORKER_ARRAY[ worker_id ].size_thresholds[current_threshold_index];
+
+	if( cotton::WORKER_ARRAY[ worker_id ].worker_deque->sizeof_deque() < current_threshold ) {
+		if( current_threshold_index > 0 ) {
+			if( cotton::WORKER_ARRAY[ worker_id ].thief_list_head->prev != NULL ) {
+				cotton::WORKER_ARRAY[ worker_id ].current_size_threshold_index -= 1;
+				DOWN(worker_id);
+			}
+		}
+	}
+}
+
+/**
+Increase the CPU frequency of the worker by one level.
+
+@param worker_id Worker
+
+@return Void
+**/
+void cotton::UP(int worker_id) {
+	return;
+}
+
+/**
+Decrease the CPU frequency of the worker by one level.
+
+@param worker_id Worker
+
+@return Void
+**/
+void cotton::DOWN(int worker_id) {
+	return;
+}
+
+/**
+Decrease the CPU frequency of Worker 2 by one level below Worker 1.
+
+@param worker_id_1 Worker 1
+@param worker_id_2 Worker 2
+
+@return Void
+**/
+void cotton::DOWN(int worker_id_1, int worker_id_2) {
+	return;
 }
--- cotton-runtime.h
+++ cotton-runtime.h
@@ -1,6 +1,6 @@
 /**
 @file
-@brief Header for COTTON runtime metohds.
+@brief Header for COTTON runtime methods.
 **/
 
 #include "cotton.h"
@@ -8,18 +8,20 @@
 
 namespace cotton {
 	
+	#define NUM_THRESHOLDS 4
 	#define CACHE_LINE_SIZE 64
 	
 	volatile bool SHUTDOWN;
-	pthread_t *thread = NULL;
+	pthread_t* thread = NULL;
 	pthread_key_t THREAD_KEY;
 	unsigned int NUM_WORKERS = 0;
-	pthread_mutex_t *DEQUE_MUTEX = NULL;
+	pthread_mutex_t* DEQUE_MUTEX = NULL;
 	volatile unsigned int FINISH_COUNTER;
 	const unsigned int MAX_DEQUE_SIZE = 100;
 	const unsigned int DEFAULT_NUM_WORKERS = 1;
 	pthread_once_t THREAD_KEY_ONCE = PTHREAD_ONCE_INIT;
 	pthread_mutex_t FINISH_MUTEX = PTHREAD_MUTEX_INITIALIZER;
+	const double CPU_FREQUENCIES_SUPPORTED[] = {2.4, 2.4, 2.4, 2.4}; // use cpupower to get the list - does not work on my system though
 
 	struct Deque {
 		volatile unsigned int head;
@@ -45,12 +47,56 @@ namespace cotton {
 		}
 
 		bool isEmpty();
+		unsigned int sizeof_deque();
 		void* pop_from_deque();
 		void* steal_from_deque();
 		void push_to_deque(void *task);
 	};
+	// Deque* DEQUE_ARRAY = NULL;
 
-	Deque *DEQUE_ARRAY = NULL;
+	struct Thief_node {
+		int thief_worker_id;
+		Thief_node* prev;
+		Thief_node* next;
+
+		Thief_node() {
+			thief_worker_id = -1;
+			prev = NULL;
+			next = NULL;
+		}
+
+		Thief_node(int thief_worker_id) : Thief_node() {
+			this->thief_worker_id = thief_worker_id;
+		}
+	};
+
+	struct Worker {
+		Deque* worker_deque;
+		int size_thresholds[NUM_THRESHOLDS];
+		int current_size_threshold_index;
+		int current_frequency_index;
+		Thief_node* thief_list_head;
+
+		Worker() {
+			worker_deque = new Deque();
+			for (int i = 0; i < NUM_THRESHOLDS; i++) {
+				size_thresholds[i] = (2 * MAX_DEQUE_SIZE * i) / (NUM_THRESHOLDS + 1);
+			}
+			current_frequency_index = 0;
+			current_size_threshold_index = 0;
+			thief_list_head = new Thief_node();
+		}
+
+		bool is_victim();
+		void add_thief(int thief_worker_id);
+		void free_thief_list();
+
+		~Worker() {
+			delete worker_deque;
+			free_thief_list();
+		}
+	};
+	Worker* WORKER_ARRAY;
 	
 	void free_all();
 	void lib_key_init();
@@ -60,4 +106,18 @@ namespace cotton {
 	unsigned int thread_pool_size();
 	void* worker_routine(void *args);
 	void push_task_to_runtime(void *task);
+
+	// Energy efficient runtime methods.
+	void UP(int worker_id);	
+	void DOWN(int worker_id);
+	void DOWN(int victim_worker_id, int thief_worker_id);
+
+	// Workpath sensitive
+	void begin_victim_thief_relationship(int victim_worker_id, int thief_worker_id);
+	void end_victim_thief_relationship(int victim_worker_id, int thief_worker_id);
+	void end_victim(int victim_worker_id);
+
+	// Workload sensitive
+	void workload_up_check(int worker_id);
+	void workload_down_check(int worker_id);
 }
